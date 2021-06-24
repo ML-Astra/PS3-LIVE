b3a0988cbf04f9f8c19e433b1f063ccd655cea5d
ML-Astra <zer0lexaros@gmail.com>
2021-06-24 19:43:00 +0100

    Main Code Re write

--- index.js
+++ index.js
@@ -13,23 +13,37 @@ cjs.onCommand = (user, command, message, flags, extra) =>{
     if(flags.broadcaster && command === "ps3-test"){
         console.log('Testing PS3 Connection');
         cjs.Say('Sent Test PS3 Message');
-        ps3Msg('Testing Connection', 1, 5);
+        ps3Msg('Testing Connection', 6, 5); //6 = Glowing Update Icon
     }
 }
 
 cjs.onChat = (user, message, flags, self, extra) =>{
-    if(message.includes('Cheers for the follow') == true){
-        ps3Msg(message, 1, 5);
+    if(message.includes('Cheers for the follow') == true && user == "StreamElements"){
+        ps3Msg(message, 1, 5); //1 = Friends Icon
         console.log(message);
     }
 }
 
+
+cjs.onReward = ( user, reward, cost, message, extra ) => {
+    if(reward == "[PS3 ONLY] Send a message via PS3 Live"){
+        ps3Msg(`${user}: ${message}`, 38, 5); //38 = Chat Bubble
+    }
+  }
+
 cjs.onSub = (user, message, subTierInfo, extra)=>{
-    ps3Msg(`Subscription from ${user}`, 12, 5);
+    ps3Msg(`Subscription from ${user}`, 12, 5); // 12 = Platnium Trophy
 }
 
 cjs.onJoin = (user,self,extra)=>{
-    ps3Msg(`${user} has joined the chat`, 12, 5);
+    ps3Msg(`${user} has joined the chat`, 0, null); // 0 = Info Icon
+}
+
+try{
+    cjs.Init(process.env.TWITCHUSER, process.env.EXTOAUTH);
+    ps3Msg('Connected to Twitch using PS3-LIVE by ML-Astra', 50, 5); //50 = Connect Icon
+}catch{
+    // It doesn't like me removing this for some reason.
 }
 // Specify these either in a .env file or as exports.
-cjs.Init(process.env.TWITCHUSER, process.env.OAUTH);
+
